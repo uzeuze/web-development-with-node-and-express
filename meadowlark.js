769f5ffd6c8c27726f81b57314c8e2c8fd836025
Egemen Can Uze <egemencanuze@gmail.com>
2016-09-19 17:34:33 +0300

    Add zombie.js for crosspage testing

--- meadowlark.js
+++ meadowlark.js
@@ -24,6 +24,13 @@ app.get('/about', function(req, res) {
   res.render('about', { fortune: fortune.getFortune(),
                         pageTestScript: '/qa/tests-about.js' } );
 });
+
+app.get('/tours/hood-river', function(req, res){
+ res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function(req, res){
+ res.render('tours/request-group-rate');
+});
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next){
  res.status(404);
