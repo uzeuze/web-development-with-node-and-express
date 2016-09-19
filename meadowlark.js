c284697868bf9173de502ae406d860b9776629c4
Egemen Can Uze <egemencanuze@gmail.com>
2016-09-19 17:07:58 +0300

    Add page tests

--- meadowlark.js
+++ meadowlark.js
@@ -9,13 +9,20 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+
+app.use(function(req, res, next){
+ res.locals.showTests = app.get('env') !== 'production' &&
+ req.query.test === '1';
+ next();
+});
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
  res.render('home');
 });
 app.get('/about', function(req, res) {
-  res.render('about', { fortune: fortune.getFortune() } );
+  res.render('about', { fortune: fortune.getFortune(),
+                        pageTestScript: '/qa/tests-about.js' } );
 });
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next){
