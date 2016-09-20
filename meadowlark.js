74d78f08156869ec21d6dadb39d6e4b051da0cb5
Egemen Can Uze <egemencanuze@gmail.com>
2016-09-20 17:14:10 +0300

    add unit test for get random fortune function

--- meadowlark.js
+++ meadowlark.js
@@ -20,11 +20,20 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
  res.render('home');
 });
+app.get('/headers', function(req,res){
+ res.set('Content-Type','text/plain');
+ var s = '';
+ for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+ res.send(s);
+});
+
 app.get('/about', function(req, res) {
   res.render('about', { fortune: fortune.getFortune(),
                         pageTestScript: '/qa/tests-about.js' } );
 });
-
+app.get('/tours/oregon-coast', function(req, res){
+ res.render('tours/hood-river');
+});
 app.get('/tours/hood-river', function(req, res){
  res.render('tours/hood-river');
 });
